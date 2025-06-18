e1a7b4c673300aec530922d2d709df0c9eebd3f3
wignn <tigfiragnafatur1933@gmail.com>
2025-06-19 05:46:15 +0700

    refactor: clean up code formatting and improve structure of POST request handling

--- src/app/api/whatsapp/route.ts
+++ src/app/api/whatsapp/route.ts
@@ -2,14 +2,12 @@ import { prisma } from "@/lib/prisma";
 import axios from "axios";
 import { NextResponse } from "next/server";
 
-
 type Status = "AMAN" | "WASPADA" | "BAHAYA";
 
 interface SensorData {
   ketinggianAir: number;
   status: Status;
 }
-
 
 interface ApiResponse {
   message?: string;
@@ -20,37 +18,36 @@ interface ApiResponse {
 export async function POST(req: Request): Promise<NextResponse<ApiResponse>> {
   try {
     const body: SensorData = await req.json();
-    const {ketinggianAir, status }: SensorData = body;
+    const { ketinggianAir, status }: SensorData = body;
 
     await prisma.report.create({
-      data:{
+      data: {
         ketinggian: Number(ketinggianAir),
         status: status,
-        expirationDate: new Date(Date.now() + 24 * 60 * 60 * 1000), 
-      }
+        expirationDate: new Date(Date.now() + 24 * 60 * 60 * 1000),
+      },
     });
-    const user = await prisma.client.findMany();
 
-    for (const u of user) {
-
-      await axios
-        .post(`${process.env.NEXT_PUBLIC_API_URL}/api/send-message`, {
-          number: u.phone,
-          message:  `⚠️ ${status.charAt(0).toUpperCase() + status.slice(1).toLowerCase()}! Ketinggian air saat ini mencapai ${ketinggianAir} cm. Harap bersiap dan perhatikan kondisi sekitar.`
-
-,
-        })
-        .catch((error) => {
-          console.error(`Gagal kirim ke ${u.phone}:`, error.message);
-        });
+    if (status == "WASPADA" || status == "BAHAYA") {
+      const user = await prisma.client.findMany();
+      for (const u of user) {
+        await axios
+          .post(`${process.env.NEXT_PUBLIC_API_URL}/api/send-message`, {
+            number: u.phone,
+            message: `⚠️ ${
+              status.charAt(0).toUpperCase() + status.slice(1).toLowerCase()
+            }! Ketinggian air saat ini mencapai ${ketinggianAir} cm. Harap bersiap dan perhatikan kondisi sekitar.`,
+          })
+          .catch((error) => {
+            console.error(`Gagal kirim ke ${u.phone}:`, error.message);
+          });
+      }
     }
-
-
 
     return NextResponse.json(
       {
         message: "Data berhasil diterima",
-        data: {ketinggianAir, status},
+        data: { ketinggianAir, status },
       },
       { status: 200 }
     );
